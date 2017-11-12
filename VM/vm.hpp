9d490d7e0cad9e35cf0140bb416a8ec46e6f7b3e
Ben Stone <ben.stone-sraha@fau.de>
2017-11-12 13:35:37 +0100

    Changed Licensing Header

--- VM/vm.hpp
+++ VM/vm.hpp
@@ -1,3 +1,23 @@
+/* License
+* --------------------------------------------------------------------------------------------------------------------
+* This file is part of the VM.
+*
+* Copyright (c) 2017 Andreas Trütschel.
+* Copyright (c) 2017 Benjamin Stone.
+*
+* AI4E is free software: you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as
+* published by the Free Software Foundation, version 3.
+*
+* AI4E is distributed in the hope that it will be useful, but
+* WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program. If not, see <http://www.gnu.org/licenses/>.
+* --------------------------------------------------------------------------------------------------------------------
+*/
 #pragma once
 
 #include <QtWidgets/QMainWindow>
